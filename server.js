ef8692fe256654b17d6d2575f83fba44f200d6be
nglong145 <nguyenlong5214@gmail.com>
2025-03-30 12:10:27 +0700

    Move secret key to .env

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 const router = require("./route");
+
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server chạy tại http://localhost:3000");
