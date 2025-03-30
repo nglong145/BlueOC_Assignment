56c2a3aee9dd3543f9593b03447c86eabe76f221
nglong145 <nguyenlong5214@gmail.com>
2025-03-30 11:30:40 +0700

    Assignment Week 7

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, I'm Long");
-});
-
+const router = require("./route");
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server chạy tại http://localhost:3000");
 });
+
+app.use("/api/v1", router);
